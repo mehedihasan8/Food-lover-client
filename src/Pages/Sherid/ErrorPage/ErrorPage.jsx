e96bedd97811200bac97b6c33fb6460b941ece9e
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-05-04 15:00:00 +0600

    Ans blog page Questotion

--- src/Pages/Sherid/ErrorPage/ErrorPage.jsx
+++ src/Pages/Sherid/ErrorPage/ErrorPage.jsx
@@ -7,16 +7,26 @@ const ErrorPage = () => {
   console.log(error.message);
   return (
     <div className="error-page-bg">
-      <div className="w-full">
-        <h1 className="text-8xl text-red-600 font-bold text-center">OPSE !</h1>
-        <h2 className="text-2xl text-red-800 font-bold text-center">
-          {error?.message}
-        </h2>
-        <Link to="/">
-          <span className="flex justify-center mt-3">
-            <button className="btn flex justify-center">Back to Home</button>
-          </span>
-        </Link>
+      <div className="w-5/6  my-6">
+        <div className="my-10">
+          <h1 className="text-7xl text-red-600 font-bold text-center">
+            <span className="text-8xl text-red-600 font-bold text-center">
+              O
+            </span>
+            OPS !
+          </h1>
+          <h1 className="text-8xl text-red-600 font-bold text-center">
+            Page Not Found !
+          </h1>
+          <h2 className="text-2xl text-red-800 font-bold text-center mt-4">
+            {error?.message}
+          </h2>
+          <Link to="/">
+            <span className="flex justify-center mt-6">
+              <button className="btn flex justify-center">Back to Home</button>
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
   );
